34d5c1e5b526f6f75e65385ce3144b7e1969a7c0
John Hernandez <johnhdz.160@gmail.com>
2023-03-22 11:22:30 -0500

    front: src file modified and JS field added

---
+++ Clase_22.03/objetos.js
@@ -0,0 +1,72 @@
+const persona = {
+    firstName: 'John',
+    lastName: 'Hernandez',
+    age: 21,
+    id: 100001234343,
+    saludar: function() {
+        return `Holaa ${this.firstName} ${this.lastName}`
+    }
+}
+
+console.log(persona)
+console.log(persona.saludar())
+console.log(persona.firstName)
+console.log(persona['lastName'])
+
+class Persona {
+    constructor({ firstName, lastName, caracteristicas }) {
+        this.firstName = firstName
+        this.lastName = lastName
+        this.caracteristicas = {
+            colorOjos: caracteristicas.colorOjos,
+            colorCabello: caracteristicas.colorCabello,
+            altura: caracteristicas.altura
+        }
+    }
+
+    saludar() {
+        return `Hola ${this.firstName} ${this.lastName}, mido ${this.caracteristicas.altura}. Mis ojos son ${this.caracteristicas.colorOjos}`
+    }
+}
+
+const newPersona = new Persona({ firstName:'Andres', lastName:'Hernandez',
+    caracteristicas: {
+        colorOjos: 'negros',
+        colorCabello: 'rojo',
+        altura: 1.80
+    } })
+console.log(newPersona)
+console.log(newPersona.saludar())
+newPersona.firstName = 'Nuevo nombre!'
+console.log(newPersona)
+console.log(newPersona.saludar())
+
+class Trabajador extends Persona {
+    constructor({ firstName, lastName, cargo, salario, caracteristicas }){
+        super({ firstName, lastName, caracteristicas })
+        this.cargo = cargo
+        this.salario = salario
+    }
+
+    getInformation() {
+        return `Soy ${this.firstName} ${this.lastName}, ${this.cargo}. Salary: ${this.salario}`
+    }
+}
+
+const trabajador = new Trabajador({ firstName:'Homer', lastName:'Jonek', cargo:'Software Engineering', salario:10000000, caracteristicas: {
+    colorCabello: 'marron',
+    colorOjos: 'azul',
+    altura: 1.90
+} })
+console.log(trabajador)
+console.log(trabajador.getInformation())
+console.log(trabajador.saludar())
+
+// ---------------------------------------------------------------------------------
+
+const vehiculo = {
+    tipo: 'Wagon',
+    color: 'Gris',
+    cv: 200,
+    
+}
